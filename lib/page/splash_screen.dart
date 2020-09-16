2c3444de9a3036fc2fce1e0dd0fc1b147fa7d11d
Souarv Sharma <souravgopal25@gmail.com>
2020-09-17 01:24:20 +0530

    Splash Screen Completed

--- lib/page/splash_screen.dart
+++ lib/page/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:user_side_ap/page/login_page.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key}) : super(key: key);
@@ -9,25 +12,39 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 5), () {
+      Navigator.push(
+          context, new MaterialPageRoute(builder: (context) => LoginPage()));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Expanded(
-            flex: 10,
-            child: Container(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(
-                    Icons.directions_railway,
-                    size: 100,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Expanded(
+                flex: 10,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.directions_railway,
+                        size: 400,
+                      ),
+                      CircularProgressIndicator(),
+                    ],
                   ),
-                  CircularProgressIndicator()
-                ],
+                ),
               ),
-            ),
+            ],
           ),
         ],
       ),
