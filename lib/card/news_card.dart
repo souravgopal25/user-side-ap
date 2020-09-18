1547256ca3c725ff8d37c54fa3aff63396ba05ff
Souarv Sharma <souravgopal25@gmail.com>
2020-09-19 04:44:25 +0530

    onTap Added to NewsCard

--- lib/card/news_card.dart
+++ lib/card/news_card.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:user_side_ap/models/news.dart';
 
 class NewsCard extends StatelessWidget {
   final Articles articles;
@@ -11,33 +13,38 @@ class NewsCard extends StatelessWidget {
       padding: const EdgeInsets.all(16.0),
       child: Column(
         children: <Widget>[
-          Card(
-            elevation: 10,
-            color: Colors.white,
-            shadowColor: Colors.grey[200],
-            borderOnForeground: true,
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(0.0),
-                  child: Image.network(articles.image),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    articles.title,
-                    style: TextStyle(fontSize: 25.0),
-                    textAlign: TextAlign.start,
+          GestureDetector(
+            onTap: () {
+              launch(articles.url);
+            },
+            child: Card(
+              elevation: 10,
+              color: Colors.white,
+              shadowColor: Colors.grey[200],
+              borderOnForeground: true,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(0.0),
+                    child: Image.network(articles.image),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
-                  child: Text(
-                    articles.description,
-                    style: TextStyle(fontSize: 18.0),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      articles.title,
+                      style: TextStyle(fontSize: 25.0),
+                      textAlign: TextAlign.start,
+                    ),
                   ),
-                )
-              ],
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                    child: Text(
+                      articles.description,
+                      style: TextStyle(fontSize: 18.0),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ],
