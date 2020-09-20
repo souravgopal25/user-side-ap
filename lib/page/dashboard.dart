3c028c7aef509b7384f154f4c45fb4a85a2b70e6
Souarv Sharma <souravgopal25@gmail.com>
2020-09-21 02:55:52 +0530

    Dashboard Ui Added

--- lib/page/dashboard.dart
+++ lib/page/dashboard.dart
@@ -6,8 +6,94 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[DashBoardSlider()],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DashBoard"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DashBoardSlider(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                //TODO button does't looks good as of now
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(colors: <Color>[
+                          Color(0xFF396AFC),
+                          Color(0xFF2948FF)
+                        ])),
+                        child: Text(
+                          "Report Anonymously",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Container(
+                        decoration: const BoxDecoration(
+                            gradient: LinearGradient(colors: <Color>[
+                          Color(0xFF396AFC),
+                          Color(0xFF2948FF)
+                        ])),
+                        child: Text(
+                          "Report",
+                          style: TextStyle(fontSize: 40, color: Colors.white),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {},
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(colors: <Color>[
+                      Color(0xFF396AFC),
+                      Color(0xFF2948FF)
+                    ])),
+                    child: Text(
+                      "Alerts",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  child: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(colors: <Color>[
+                      Color(0xFF396AFC),
+                      Color(0xFF2948FF)
+                    ])),
+                    child: Text(
+                      "News",
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
