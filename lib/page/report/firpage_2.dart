38a2f13ad72eddd1a42e5c581f4d4f182f2d8be3
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-09-21 20:40:36 +0530

    FIR page 2 with information about Case type, Case Details, Desription etc.

---
+++ lib/page/report/firpage_2.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+
+class CaseDetails extends StatefulWidget {
+  CaseDetails({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  _CaseDetailsPage createState() => _CaseDetailsPage();
+
+}
+
+class _CaseDetailsPage extends State<CaseDetails> {
+
+  String titleCase;
+  String description;
+  String suspect;
+
+  Widget build (BuildContext context) {
+    return Scaffold (
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Case Type',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        )
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      titleCase = value;
+                    });
+                  },
+
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Case Title',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        )
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      titleCase = value;
+                    });
+                  },
+
+                ),
+              ),
+
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: TextField(
+                  minLines: 2,
+                  maxLines: 10,
+                  decoration: InputDecoration(
+                    labelText: 'Case Description',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        )
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      description = value;
+                    });
+                  },
+
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Suspects(if any)',
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Colors.blue,
+                        )
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      suspect = value;
+                    });
+                  },
+
+                ),
+              ),
+
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 8.0),
+                child: MaterialButton(
+                  child: Text('Submit',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),),
+                  color: Colors.blue,
+                  onPressed: () {
+
+                    print(titleCase);
+                    print(description);
+                    print(suspect);
+
+                  },
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        elevation: 5.0,
+        child: new Icon(Icons.chevron_right),
+        backgroundColor: Colors.blue,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (BuildContext context) => (AnonymousRegistration())
+          ));
+        },
+      ),
+    );
+
+  }
+
+}
